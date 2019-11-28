1630174e8c4a2e5174d137d7d65f817c4e3dc558
takahiro.sawakami <takahirosawakami81@gmail.com>
2019-11-28 05:30:59 +0000

    fix missing characters

--- resources/views/user_follow/follow_button.blade.php
+++ resources/views/user_follow/follow_button.blade.php
@@ -1,5 +1,5 @@
 @if (Auth::id() != $user->id)
-    @if (Auth::user()->is_following($user->id)
+    @if (Auth::user()->is_following($user->id))
         {!! Form::open(['route' => ['user.unfollow', $user->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfollow', ['class' => "btn btn-danger btn-block"]) !!}
         {!! Form::close() !!}
